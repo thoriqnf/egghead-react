231d74c039ffafa944aefe332ffc38d861fc1db6
thoriqnf <thoriqnfaizal@gmail.com>
2017-11-01 10:31:04 +0700

    end lesson 6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,11 +17,15 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <input type="text" onChange={this.update.bind(this)} />
         <h1>{this.state.txt}</h1>;
+        <Widget update={this.update.bind(this)} />
+        <Widget update={this.update.bind(this)} />
+        <Widget update={this.update.bind(this)} />
       </div>
     );
   }
 }
+
+const Widget = props => <input type="text" onChange={props.update} />;
 
 export default App;
