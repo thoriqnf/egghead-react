6c6fb5a04223488209416411dcceb37b28db81dc
thoriqnf <thoriqnfaizal@gmail.com>
2017-11-02 15:31:54 +0700

    end lesson 11 mount and unmount

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,46 @@
 import React from "react";
+import ReactDOM from "react-dom";
+
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {val: 0};
-    this.update = this.update.bind(this)
+    this.state = { val: 0 };
+    this.update = this.update.bind(this);
   }
-  update(){
-    this.setState(){val: this.state.val + 1})
+  update() {
+    this.setState({ val: this.state.val + 1 });
+  }
+  componentWillMount() {
+    console.log("componentWillMount");
+  }
+  render() {
+    console.log("render");
+    return <button onClick={this.update}>{this.state.val}</button>;
+  }
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
   }
 }
 
-export default App;
+class Wrapper extends React.Component {
+  mount() {
+    ReactDOM.render(<App />, document.getElementById("a"));
+  }
+  unmount() {
+    ReactDOM.unmountComponentAtNode(document.getElementById("a"));
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unmount.bind(this)}>UnMount</button>
+        <div id="a" />
+      </div>
+    );
+  }
+}
+
+export default Wrapper;
