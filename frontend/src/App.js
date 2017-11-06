e03f1345b6818763296b0412a886430ea866d778
thoriqnf <thoriqnfaizal@gmail.com>
2017-11-06 12:44:43 +0700

    end lesson 19

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,37 @@
 import React from "react";
-import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
-      <Parent>
-        <div className="childA" />
-      </Parent>
+      <Buttons>
+        <button value="A">A</button>
+        <button value="B">B</button>
+        <button value="C">C</button>
+      </Buttons>
     );
   }
 }
 
-class Parent extends React.Component {
+class Buttons extends React.Component {
+  constructor() {
+    super();
+    this.state = { selected: "None" };
+  }
+  selectItem(selected) {
+    this.setState({ selected });
+  }
   render() {
-    // console.log(this.props.children);
-    // let items = React.Children.forEach(this.props.children, child =>
-    //   console.log(child.props.className)
-    // );
-    // let items = React.Children.toArray(this.props.children);
-    let items = React.Children.only(this.props.children);
-    console.log(items);
-    return null;
+    let fn = child =>
+      React.cloneElement(child, {
+        onClick: this.selectItem.bind(this, child.props.value)
+      });
+    let items = React.Children.map(this.props.children, fn);
+    return (
+      <div>
+        <h2>You have selected: {this.state.selected}</h2>
+        {items}
+      </div>
+    );
   }
 }
 
