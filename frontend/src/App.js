26e59e9be10583a786ccafd9653dba4d518850c7
thoriqnf <thoriqnfaizal@gmail.com>
2017-11-06 20:31:20 +0700

    end lesson 20 carefull depricated if you use react v 16

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,81 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 class App extends React.Component {
-  render() {
-    return (
-      <Buttons>
-        <button value="A">A</button>
-        <button value="B">B</button>
-        <button value="C">C</button>
-      </Buttons>
-    );
-  }
-}
-
-class Buttons extends React.Component {
   constructor() {
     super();
-    this.state = { selected: "None" };
+    this.state = {
+      red: 0
+    };
+    this.update = this.update.bind(this);
   }
-  selectItem(selected) {
-    this.setState({ selected });
+  update(e) {
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value
+    });
   }
   render() {
-    let fn = child =>
-      React.cloneElement(child, {
-        onClick: this.selectItem.bind(this, child.props.value)
-      });
-    let items = React.Children.map(this.props.children, fn);
     return (
       <div>
-        <h2>You have selected: {this.state.selected}</h2>
-        {items}
+        <NumInput
+          ref="red"
+          min={0}
+          max={255}
+          step={0.01}
+          val={+this.state.red}
+          label="Red"
+          update={this.update}
+        />
       </div>
     );
   }
 }
+
+class NumInput extends React.Component {
+  render() {
+    let label =
+      this.props.label !== "" ? (
+        <label>
+          {this.props.label} - {this.props.val}
+        </label>
+      ) : (
+        ""
+      );
+    return (
+      <div>
+        <input
+          ref="inp"
+          type={this.props.type}
+          min={this.props.min}
+          max={this.props.max}
+          step={this.props.step}
+          defaultValue={this.props.val}
+          onChange={this.props.update}
+        />
+        {label}
+      </div>
+    );
+  }
+}
+
+NumInput.propTypes = {
+  min: PropTypes.number,
+  max: PropTypes.number,
+  step: PropTypes.number,
+  val: PropTypes.number,
+  label: PropTypes.string,
+  update: PropTypes.func.isRequired,
+  type: PropTypes.oneOf(["number", "range"])
+};
+
+NumInput.defaultProps = {
+  min: 0,
+  max: 0,
+  step: 1,
+  val: 0,
+  label: "",
+  type: "range"
+};
 
 export default App;
